b29108f7c98b50e6b635ec473b2f215d08f567c3
alfons <alfons98@live.com>
2023-09-28 13:22:58 +0200

    plot_temperatures now CLI

--- plot_temperatures.py
+++ plot_temperatures.py
@@ -1,26 +1,37 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+import click
+
 import my_statistics as stats
 
 def read_temperatures_from_file(file_name: str, num_measurements: int, column_name: str):
     data = pd.read_csv(file_name, nrows=num_measurements)
     return data[column_name]
 
-def plot_temperatures(temperatures: list[float], mean: float, num_measurements: int) -> None:
+def plot_temperatures(temperatures: list[float], mean: float, num_measurements: int, output_file) -> None:
     plt.xlabel("Number of measurement")
     plt.ylabel("Air temperatures")
     plt.plot(temperatures, "r-")
     plt.axhline(y=mean, color="b", linestyle="--")
-    plt.savefig(f"{num_measurements}.png")
+    plt.savefig(output_file)
     plt.show()
     plt.clf()
 
-for num_measurements in [25, 100, 500]:
+@click.command()
+@click.option('--num_measurements', help='Number of temperatures to read and plot.', type=int, required = True)
+@click.option('--inputfile', help='The input data file', required=True)
+@click.option('--outputfile', help='The output image file', required=True)
+
+def main(num_measurements, inputfile, outputfile):
+
     # read data from file
-    temperatures = read_temperatures_from_file("temperatures.csv", num_measurements, "Air temperature (degC)")
+    temperatures = read_temperatures_from_file(inputfile, num_measurements, "Air temperature (degC)")
 
     # compute statistics
     mean = stats.compute_statistics(temperatures) 
 
     # plot results
-    plot_temperatures(temperatures, mean, num_measurements)
+    plot_temperatures(temperatures, mean, num_measurements, outputfile)
+
+if __name__ == "__main__":
+    main()
